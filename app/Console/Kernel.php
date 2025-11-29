df5a5205833da16f235ae63a966314d4aac56075
brucefern <171006308@qq.com>
2025-11-29 16:54:09 +0800

    修改定时任务

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -38,14 +38,14 @@ class Kernel extends ConsoleKernel
 
         // 每天在 8:00-12:00 随机时间执行
         $schedule->command('sign:auto')
-            // ->dailyAt('8:00') // 基准时间
-            // ->between('8:00', '12:00') // 确保在时间范围内
-            // ->when(function () {
-            //     // 添加 0-4 小时的随机延迟（确保在8-12点之间）
-            //     $randomDelay = random_int(0, 240); // 随机分钟数 (0-240分钟)
-            //     sleep($randomDelay * 60);
-            //     return true;
-            // })
+            ->dailyAt('8:00') // 基准时间
+            ->between('8:00', '12:00') // 确保在时间范围内
+            ->when(function () {
+                // 添加 0-4 小时的随机延迟（确保在8-12点之间）
+                $randomDelay = random_int(0, 240); // 随机分钟数 (0-240分钟)
+                sleep($randomDelay * 60);
+                return true;
+            })
             ->withoutOverlapping() // 防止任务重叠
             ->appendOutputTo(storage_path('logs/sign.log')) // 记录日志
             ->name('sign_auto')
